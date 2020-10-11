33dd4bc0cc39e5094f423ee801f8e77e61ee425e
Sachin Dangayach <sachin.dangayach@gmail.com>
2020-10-11 12:57:08 +0530

    Session 11 submission -24

--- session11/models/session11_train_model.py
+++ session11/models/session11_train_model.py
@@ -9,7 +9,7 @@ from torch.optim.lr_scheduler import StepLR
 
 from tqdm import tqdm
 
-def train(model, device, train_loader, criterion, optimizer, epoch, train_losses, train_acc):
+def train(model, device, train_loader, criterion, optimizer, epoch, train_losses, train_acc, scheduler):
     """Train network"""
     model.train()
     pbar = tqdm(train_loader)
@@ -33,6 +33,8 @@ def train(model, device, train_loader, criterion, optimizer, epoch, train_losses
         # Backpropagation
         loss.backward()
         optimizer.step()
+
+        scheduler.step()
 
         # Update pbar-tqdm
 
